17051947f075db898b1341b034bd7065304068bd
helena rodrigues <helena19.rodrigues@gmail.com>
2022-07-05 19:46:17 +0200

    ejerciciosJs

---
+++ sintaxisJs/exercises.js
@@ -0,0 +1,16 @@
+// ejercicio 1
+console.log("¡Empezamos con javascript!");
+// ejercicio 3
+console.log("Hola, soy un estudiante Hackademy.");
+
+//ejercicio 4-Decalarar una variable y reasignarla
+let nombre = "Jussara";
+nombre = "Helena";
+console.log(nombre);
+
+//ejercicio 5 Declara dos variables que al imprimirlas formen una frase.
+
+let saludo = "Hola,";
+let presentar = "me llamo Helena";
+console.log(saludo + presentar);
+
